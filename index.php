6541dbdc96dd46fe76f0ffb5713a861921b9b350
pakdot05 <125708652+pakdot05@users.noreply.github.com>
2024-11-12 18:23:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GYM</title> 
     <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-
+<?php 
+    require('inc/links.php');
+?>
     <!-- LINKS -->
     <style>
 
